0d30197980bd42929261f09fca9c04a1f92dca02
Subiksha Shrestha <subikshashrestha27@gmail.com>
2021-10-10 17:33:10 -0500

    [DEVELOP] Added Lost and Found Component with card

--- src/pages/lostandfound.page/lostandfound.page.jsx
+++ src/pages/lostandfound.page/lostandfound.page.jsx
@@ -1,10 +1,39 @@
-import React from "react";
-import { Text } from "react-native-paper";
+import React, { useState, useEffect } from "react";
+import { FlatList, SafeAreaView, View } from "react-native";
+import { TextInput } from "react-native-paper";
+import CardComponent from "../../components/card.component/card.component";
+
+import data from "./sample.data";
 
 const LostAndFoundPage = () => {
-    return (
-        <Text>Hello World</Text>
-    )
-}
+  const [searchData, setSearchData] = useState(data);
+  
+  useEffect(() => {
+    //TO DO: Api call to search lost and found data
+  },[]);
+  
+  const searchDataCollection = (text) => {
+    setSearchData(data.filter((each) => each.name.includes(text)));
+  };
+  const renderItem = ({ item }) => <CardComponent {...item} />;
+  return (
+    <View>
+      <View style={{ width: 400 }}>
+        <TextInput
+          label="Search Lost and Found"
+          onChangeText={(text) => searchDataCollection(text)}
+        />
+      </View>
+
+      <SafeAreaView style={{flex:1}}>
+        <FlatList
+          data={searchData}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
+    </View>
+  );
+};
 
 export default LostAndFoundPage;
